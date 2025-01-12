061402f381c2dc0d8d20541c45fa6ce187f73e8b
HeidiSiao <heidi.siao@gmail.com>
2025-01-13 05:42:25 +0800

    add-test2-over

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <TryOne />
+  <hr />
+  <TryTwo />
 </template>
 
 <script setup>
-import TryOne from './components/TryOne.vue'
+import TryOne from './components/TryOne.vue';
+import TryTwo from './components/TryTwo.vue';
 </script>
+
+<style>
+code {
+  color: salmon;
+}
+</style>
